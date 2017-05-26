9f269644430e657296ecf65d407c6c7d38af39cd
faker <wangzhipeng2817@tops001.com>
2017-05-26 11:40:34 +0800

    web层集成dao层

--- service-notify-boot/src/main/java/com/faker/controller/SmsController.java
+++ service-notify-boot/src/main/java/com/faker/controller/SmsController.java
@@ -2,12 +2,12 @@ package com.faker.controller;
 
 import com.faker.api.SmsAPI;
 import com.faker.model.SmsParam;
+import com.faker.notify.dao.SmsMapper;
+import com.faker.notify.entity.Sms;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by faker on 17/5/24.
@@ -18,9 +18,17 @@ public class SmsController implements SmsAPI {
     @Value("${notify.name}")
     private String serviceName;
 
+    @Autowired
+    private SmsMapper smsMapper;
 
     @Override
     public String sendSms(@RequestBody SmsParam smsParam) {
         return "I am "+ serviceName+ ", phone: " + smsParam.getPhone();
+    }
+
+    @RequestMapping("/sms/{smsId}")
+    @ResponseBody
+    public Sms getSms(@PathVariable Long smsId){
+        return smsMapper.selectByPrimaryKey(smsId);
     }
 }
